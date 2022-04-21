8cdc60616aa310cd3c20c0ef1a10f3de99cbe387
VigneshS02121994 <vigneshitvijay@gmail.com>
2022-04-21 21:19:41 +0530

    company details

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,20 @@
+package org.company;
+
+public class CompanyInfo {
+	public void companyname() {
+		System.out.println("company name is Biotech");
+	}
+	public void companyid() {
+		System.out.println("company id is 56342");
+	}
+	public void companyaddress() {
+		System.out.println("companyaddress is Banglore");
+	}
+	public static void main(String[]args) {
+		CompanyInfo l = new CompanyInfo();
+		l.companyname();
+		l.companyid();
+		l.companyaddress();
+	}
+}
+		
